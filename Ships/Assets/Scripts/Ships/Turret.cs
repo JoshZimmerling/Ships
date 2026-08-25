43fbeb35bfde97906b01c988c566884f77a7f1b6
jslekcin1 <120544523+jslekcin1@users.noreply.github.com>
2026-08-24 17:19:18 -0700

    Added Bullet Randomness

--- Ships/Assets/Scripts/Ships/Turret.cs
+++ Ships/Assets/Scripts/Ships/Turret.cs
@@ -22,8 +22,11 @@ public class Turret : NetworkBehaviour
     [SerializeField] private int damage;
     [SerializeField] private float fireRate;
     [SerializeField] private int range;
-    [SerializeField] private int aimPoint;
-    [SerializeField] private int arcSpread;
+
+    [SerializeField] private int aimDirection;
+    [SerializeField] private int firingArc;
+    [SerializeField] private int firingSpread;
+
     [SerializeField] private int projectileSpeed;
     [SerializeField] private float counter = 0;
 
@@ -40,7 +43,7 @@ public class Turret : NetworkBehaviour
 
 
     float maxRadians;
-    Vector2 fireAngle;
+    Vector2 fireVector;
 
     float distanceToTarget;
 
@@ -56,8 +59,8 @@ public class Turret : NetworkBehaviour
         bestTarget = null;
         closestShipDistance = Mathf.Infinity;
 
-        maxRadians = (aimPoint + transform.rotation.eulerAngles.z) * Mathf.Deg2Rad;
-        fireAngle = new Vector2(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians));
+        maxRadians = (aimDirection + transform.rotation.eulerAngles.z) * Mathf.Deg2Rad;
+        fireVector = new Vector2(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians));
 
         foreach (GameObject enemyShip in GameSceneManager.Singleton.shipsInScene)
         {
@@ -76,27 +79,19 @@ public class Turret : NetworkBehaviour
             Vector2 targetedPos = new Vector2(bestTarget.transform.position.x, bestTarget.transform.position.y);// + bestTarget.GetComponent<Movement>().GetFuturePosition(timeToTarget); //TODO: FIX
 
             // Determine the shot direction
-            Vector2 shootDirection = (targetedPos - new Vector2(transform.position.x, transform.position.y)).normalized;
+            Vector2 shootDirection = (targetedPos - new Vector2(transform.position.x, transform.position.y));
+            float angleDiff = Vector2.SignedAngle(shootDirection, fireVector);
 
-            //Clamping shot angle to inside the bounds of our spread
-            if (arcSpread != 360)
-            {
-                Vector2 leftEdgeVector = RotateVector(fireAngle, arcSpread * 0.5f).normalized;
-                Vector2 rightEdgeVector = RotateVector(fireAngle, -arcSpread * 0.5f).normalized;
+            // Clamping shot angle to inside the bounds of our spread
+            if (firingArc != 360 && Mathf.Abs(angleDiff) > firingArc / 2)
+                angleDiff = firingArc / 2 * Mathf.Sign(angleDiff);
 
-                if (leftEdgeVector.x < rightEdgeVector.x)
-                    shootDirection.x = Mathf.Clamp(shootDirection.x, leftEdgeVector.x, rightEdgeVector.x);
-                else
-                    shootDirection.x = Mathf.Clamp(shootDirection.x, rightEdgeVector.x, leftEdgeVector.x);
+            // Calculate final fiire angle
+            float fireAngle = (aimDirection + transform.rotation.eulerAngles.z - angleDiff + Random.Range(-firingSpread / 2, firingSpread / 2)) * Mathf.Deg2Rad;
+            fireVector = new Vector2(Mathf.Cos(fireAngle), Mathf.Sin(fireAngle));
 
-                if (leftEdgeVector.y < rightEdgeVector.y)
-                    shootDirection.y = Mathf.Clamp(shootDirection.y, leftEdgeVector.y, rightEdgeVector.y);
-                else
-                    shootDirection.y = Mathf.Clamp(shootDirection.y, rightEdgeVector.y, leftEdgeVector.y);
-            }
-
-            //Fire the bullet at the angle calculated
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(new Vector3(0, 0, 1), shootDirection));
+            // Fire the bullet at the angle calculated
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(new Vector3(0, 0, 1), fireVector));
             bullet.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
             bullet.GetComponent<Bullet>().SetupBullet(range / projectileSpeed, damage, projectileSpeed);
             bullet.transform.parent = GameSceneManager.Singleton.bulletContainer;
@@ -111,17 +106,17 @@ public class Turret : NetworkBehaviour
         int corrVal = enemyShip.GetComponent<Ship>().correctionFactor;
         Vector2 delta = enemyShip.transform.position - transform.position;
         distanceToTarget = delta.magnitude - corrVal;
-        if (distanceToTarget > range) return false; // Is out of range     d^2 > (r+c)^2     = d > r+c = (d-c) > r = (d-c)^2 > r^2
+        if (distanceToTarget > range) return false; // Is out of range
 
-        if (arcSpread == 360) return true;
+        if (firingArc == 360) return true;
 
-        float dot = Vector2.Dot(delta.normalized, fireAngle);
-        float halfAngleRad = (arcSpread * 0.5f) * Mathf.Deg2Rad;
+        float dot = Vector2.Dot(delta.normalized, fireVector);
+        float halfAngleRad = (firingArc * 0.5f) * Mathf.Deg2Rad;
         float cosHalfAngle = Mathf.Cos(halfAngleRad);
         if (dot >= cosHalfAngle && distanceToTarget <= range) return true; // Center of ship in sector (extended)
 
-        Vector2 leftEdgeDir = RotateVector(fireAngle, arcSpread * 0.5f).normalized;
-        Vector2 rightEdgeDir = RotateVector(fireAngle, -arcSpread * 0.5f).normalized;
+        Vector2 leftEdgeDir = RotateVector(fireVector, firingArc * 0.5f).normalized;
+        Vector2 rightEdgeDir = RotateVector(fireVector, -firingArc * 0.5f).normalized;
 
         if (LineSegmentIntersectsCircle(transform.position, (Vector2)transform.position + leftEdgeDir * range, enemyShip.transform.position, corrVal)) return true;
         if (LineSegmentIntersectsCircle(transform.position, (Vector2)transform.position + rightEdgeDir * range, enemyShip.transform.position, corrVal)) return true;
@@ -160,14 +155,7 @@ public class Turret : NetworkBehaviour
 
     private void OnDrawGizmos()
     {
-        //if (!IsLocalPlayer) return;
-
         float shipRotationZ = gameObject.transform.rotation.eulerAngles.z;
-
-        Gizmos.color = Color.whiteSmoke;
-        float aim = aimPoint + shipRotationZ;
-        float maxRadians = aim * Mathf.Deg2Rad;
-        //Gizmos.DrawLine(transform.position, transform.position + new Vector3(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians)) * range);
 
         switch (turretType)
         {
@@ -182,19 +170,36 @@ public class Turret : NetworkBehaviour
                 break;
         }
 
-        float max = aimPoint + arcSpread / 2 + shipRotationZ;
-        maxRadians = max * Mathf.Deg2Rad;
+        float max = aimDirection + firingArc / 2 + shipRotationZ;
+        float maxRadians = max * Mathf.Deg2Rad;
         Gizmos.DrawLine(transform.position, transform.position + new Vector3(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians)) * range);
 
-        float min = aimPoint - arcSpread / 2 + shipRotationZ;
+        float min = aimDirection - firingArc / 2 + shipRotationZ;
         float minRadians = min * Mathf.Deg2Rad;
         Gizmos.DrawLine(transform.position, transform.position + new Vector3(Mathf.Cos(minRadians), Mathf.Sin(minRadians)) * range);
 
-        for (float a = min; a < max; a += 5)
+        for (float a = min; a < max - 1; a += 5)
         {
             minRadians = a * Mathf.Deg2Rad;
             maxRadians = (a + 5) * Mathf.Deg2Rad;
             Gizmos.DrawLine(transform.position + new Vector3(Mathf.Cos(minRadians), Mathf.Sin(minRadians)) * range, transform.position + new Vector3(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians)) * range);
+        }
+
+        // Draw spread
+        Gizmos.color = Color.white;
+
+        max = aimDirection + firingSpread / 2 + shipRotationZ;
+        maxRadians = max * Mathf.Deg2Rad;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians)) * 10);
+        min = aimDirection - firingSpread / 2 + shipRotationZ;
+        minRadians = min * Mathf.Deg2Rad;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(Mathf.Cos(minRadians), Mathf.Sin(minRadians)) * 10);
+
+        for (float a = min; a < max; a += 1)
+        {
+            minRadians = a * Mathf.Deg2Rad;
+            maxRadians = (a + 1) * Mathf.Deg2Rad;
+            Gizmos.DrawLine(transform.position + new Vector3(Mathf.Cos(minRadians), Mathf.Sin(minRadians)) * 10, transform.position + new Vector3(Mathf.Cos(maxRadians), Mathf.Sin(maxRadians)) * 10);
         }
     }
 }
