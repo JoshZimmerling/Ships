c0da323380d4dda5cc0626a5988391d2b885136f
jslekcin1 <120544523+jslekcin1@users.noreply.github.com>
2026-09-12 16:20:26 -0700

    Mothership Healing Ability

--- Ships/Assets/Scripts/Ships/Ship.cs
+++ Ships/Assets/Scripts/Ships/Ship.cs
@@ -6,7 +6,7 @@ public class Ship : NetworkBehaviour
     public enum ShipTypes
     {
         Destroyer,
-        Maurader,
+        Marauder,
         Hawk,
         Challenger,
         Goliath,
@@ -33,6 +33,13 @@ public class Ship : NetworkBehaviour
     private GameObject minimapMarker; // Market on minimap (Both)
     private GameObject minimapScoutMarker; // Marker in minimap fog of war (Enemy)
 
+    // Mothership ability settings
+    private int mothershipHealRadius = 20;
+    private int mothershipHealTimer = 2;
+    private int mothershipHealAmount = 1;
+    private float abilityTimer = 0;
+
+    [SerializeField] GameObject popupTextPrefab;
     public override void OnNetworkSpawn()
     {
         playerData = PlayerDataList.Singleton.players[OwnerClientId];
@@ -49,6 +56,14 @@ public class Ship : NetworkBehaviour
         currentShipHP.OnValueChanged += (float previousValue, float newValue) => {
             hpBar.transform.localScale = new Vector3(currentShipHP.Value / maxShipHP, 1, 1);
             hpBar.transform.localPosition = new Vector3((currentShipHP.Value / maxShipHP * 0.5f) - 0.5f, 0, 0);
+
+            if (IsOwner && newValue > previousValue)
+            {
+                PopupText popupText = Instantiate(popupTextPrefab, transform.position + new Vector3(-1, 0) * correctionFactor * 0.5f, Quaternion.identity).GetComponent<PopupText>();
+                popupText.SetupText("+", Color.greenYellow, 1f);
+                popupText = Instantiate(popupTextPrefab, transform.position + new Vector3(1, -1f) * correctionFactor * 0.5f, Quaternion.identity).GetComponent<PopupText>();
+                popupText.SetupText("+", Color.greenYellow, 1f);
+            }
         };
 
         // Changes based on ship owner
@@ -100,6 +115,10 @@ public class Ship : NetworkBehaviour
                     transform.Find("Scout Radar").gameObject.SetActive(true);
                 break;
             case ShipTypes.Mothership:
+                if (IsOwner)
+                    transform.Find("Heal Aura").localScale = new Vector3(mothershipHealRadius / 6.5f, mothershipHealRadius / 6.5f);
+                else
+                    transform.Find("Heal Aura").gameObject.SetActive(false);
                 foreach (var (id, player) in PlayerDataList.Singleton.players)
                     if (player.OwnerClientId == OwnerClientId)
                         player.SetMothership(this);
@@ -111,12 +130,34 @@ public class Ship : NetworkBehaviour
     {
         switch (shipType)
         {
+            case ShipTypes.Mothership:
+                if (!IsHost) return;
+                abilityTimer -= Time.deltaTime;
+                if (abilityTimer < 0)
+                {
+                    foreach (GameObject go in GameSceneManager.Singleton.shipsInScene)
+                    {
+                        Ship ship = go.GetComponent<Ship>();
+                        if (ship != null &&
+                            ship.OwnerClientId == OwnerClientId && 
+                            ship.shipType != ShipTypes.Mothership && 
+                            (transform.position - go.transform.position).magnitude + ship.correctionFactor <= mothershipHealRadius &&
+                            ship.currentShipHP.Value != ship.maxShipHP)
+                        {
+                            ship.currentShipHP.Value = Mathf.Min(ship.currentShipHP.Value + mothershipHealAmount, ship.maxShipHP);
+                        }
+                    }
+                    abilityTimer = mothershipHealTimer;
+                }
+                break;
+            /*
             case ShipTypes.Goliath:
                 if (!IsHost) return;
-                
+
                 if (currentShipHP.Value < maxShipHP)
                     currentShipHP.Value += 1 * Time.deltaTime;
                 break;
+            */
         }
     }
 
